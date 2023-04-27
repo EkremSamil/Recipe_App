19b4c82bc6cb2a7c1685c122063ebd358fb87e9c
Ekrem Şamil Bolat <ekremsamil.1@outlook.com>
2023-04-27 22:44:17 +0300

    Initial commit

---
+++ recipe_app/lib/components/author_card.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/components/circle_image.dart';
+import 'package:recipe_app/fooderlich_theme.dart';
+
+class AuthorCard extends StatefulWidget {
+  final String authorName;
+  final String title;
+  final ImageProvider? imageProvider;
+
+  const AuthorCard({
+    super.key,
+    required this.authorName,
+    required this.title,
+    this.imageProvider,
+  });
+
+  @override
+  AuthorCardState createState() => AuthorCardState();
+}
+
+class AuthorCardState extends State<AuthorCard> {
+  bool _isFavorited = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              CircleImage(
+                imageProvider: widget.imageProvider,
+                imageRadius: 28,
+              ),
+              const SizedBox(width: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.authorName,
+                    style: FooderlichTheme.lightTextTheme.headline2,
+                  ),
+                  Text(
+                    widget.title,
+                    style: FooderlichTheme.lightTextTheme.headline3,
+                  )
+                ],
+              ),
+            ],
+          ),
+          IconButton(
+            icon: Icon(_isFavorited ? Icons.favorite : Icons.favorite_border),
+            iconSize: 30,
+            color: Colors.red[400],
+            onPressed: () {
+              setState(() {
+                _isFavorited = !_isFavorited;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
