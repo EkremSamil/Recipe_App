f209852e23184e3aae2f4adc8b4c425fbebf13c1
Ekrem Şamil Bolat <ekremsamil.1@outlook.com>
2023-05-02 04:44:41 +0300

    grocery_tile

--- recipe_app/lib/components/grocery_tile.dart
+++ recipe_app/lib/components/grocery_tile.dart
@@ -1,28 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
-import 'package:recipe_app/models/grocery_item.dart';
+import '../models/grocery_item.dart';
 
 class GroceryTile extends StatelessWidget {
   final GroceryItem item;
   final Function(bool?)? onComplete;
   final TextDecoration textDecoration;
+
   GroceryTile({
     super.key,
-    this.onComplete,
     required this.item,
+    this.onComplete,
   }) : textDecoration = item.isComplete ? TextDecoration.lineThrough : TextDecoration.none;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 5.0,
+                color: item.color,
+              ),
+              const SizedBox(width: 16.0),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.name,
+                    style: GoogleFonts.lato(
+                      decoration: textDecoration,
+                      fontSize: 21.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 4.0),
+                  buildDate(),
+                  const SizedBox(height: 4.0),
+                  buildImportance(),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                item.quantity.toString(),
+                style: GoogleFonts.lato(
+                  decoration: textDecoration,
+                  fontSize: 21.0,
+                ),
+              ),
+              buildCheckbox(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 
   Widget buildImportance() {
     if (item.importance == Importance.low) {
       return Text(
         'Low',
-        style: GoogleFonts.lato(decoration: textDecoration),
+        style: GoogleFonts.lato(
+          decoration: textDecoration,
+        ),
       );
     } else if (item.importance == Importance.medium) {
       return Text(
         'Medium',
-        style: GoogleFonts.lato(fontWeight: FontWeight.w800, decoration: textDecoration),
+        style: GoogleFonts.lato(
+          fontWeight: FontWeight.w800,
+          decoration: textDecoration,
+        ),
       );
     } else if (item.importance == Importance.high) {
       return Text(
@@ -47,46 +104,10 @@ class GroceryTile extends StatelessWidget {
     );
   }
 
-  Widget buildCheckBox() {
+  Widget buildCheckbox() {
     return Checkbox(
       value: item.isComplete,
       onChanged: onComplete,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(width: 5.0, color: item.color),
-        const SizedBox(width: 16.0),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              item.name,
-              style: GoogleFonts.lato(decoration: textDecoration, fontSize: 21.0, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 4.0),
-            buildDate(),
-            const SizedBox(height: 4.0),
-            buildImportance(),
-          ],
-        ),
-        Row(
-          children: [
-            Text(
-              item.quantity.toString(),
-              style: GoogleFonts.lato(
-                decoration: textDecoration,
-                fontSize: 21.0,
-              ),
-            ),
-            buildCheckBox(),
-          ],
-        ),
-      ],
     );
   }
 }
