a8925da28695e77ed44edc63ac7e8fc0ded7046b
Ekrem Şamil Bolat <ekremsamil.1@outlook.com>
2023-05-03 04:38:50 +0300

    .

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -1,34 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:recipe_app/models/grocery_manager.dart';
-import 'package:recipe_app/models/tab_manager.dart';
+import 'package:recipe_app/models/profile_manager.dart';
 
 import 'fooderlich_theme.dart';
-import 'home.dart';
+import 'models/models.dart';
+import 'navigation/app_router.dart';
 
-void main() {
-  runApp(const Fooderlich());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appStateManager = AppStateManager();
+  appStateManager.initializeApp();
+  runApp(Fooderlich(appStateManager: appStateManager));
 }
 
-class Fooderlich extends StatelessWidget {
-  const Fooderlich({super.key});
+class Fooderlich extends StatefulWidget {
+  final AppStateManager appStateManager;
+
+  const Fooderlich({
+    super.key,
+    required this.appStateManager,
+  });
+
+  @override
+  FooderlichState createState() => FooderlichState();
+}
+
+class FooderlichState extends State<Fooderlich> {
+  late final _groceryManager = GroceryManager();
+  late final _profileManager = ProfileManager();
+  late final _appRouter = AppRouter(
+    widget.appStateManager,
+    _profileManager,
+    _groceryManager,
+  );
+
   @override
   Widget build(BuildContext context) {
-    final theme = FooderlichTheme.light();
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: theme,
-      title: 'Fooderlich',
-      home: MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (context) => TabManager(),
-          ),
-          ChangeNotifierProvider(
-            create: (context) => GroceryManager(),
-          ),
-        ],
-        child: const Home(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => _groceryManager,
+        ),
+        ChangeNotifierProvider(
+          create: (context) => _profileManager,
+        ),
+        ChangeNotifierProvider(
+          create: (context) => widget.appStateManager,
+        ),
+      ],
+      child: Consumer<ProfileManager>(
+        builder: (context, profileManager, child) {
+          ThemeData theme;
+          if (profileManager.darkMode) {
+            theme = FooderlichTheme.dark();
+          } else {
+            theme = FooderlichTheme.light();
+          }
+
+          final router = _appRouter.router;
+
+          return MaterialApp.router(
+            theme: theme,
+            title: 'Fooderlich',
+            routerDelegate: router.routerDelegate,
+            routeInformationParser: router.routeInformationParser,
+            routeInformationProvider: router.routeInformationProvider,
+          );
+        },
       ),
     );
   }
