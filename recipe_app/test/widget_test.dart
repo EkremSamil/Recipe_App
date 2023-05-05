8fb6f35e10833a043a86a7e60d2ca537ecd5a5dd
Ekrem Şamil Bolat <ekremsamil.1@outlook.com>
2023-05-05 12:37:10 +0300

    .

--- recipe_app/test/widget_test.dart
+++ recipe_app/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:recipe_app/screens/home.dart';
+import 'package:fooderlich/screens/screens.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
